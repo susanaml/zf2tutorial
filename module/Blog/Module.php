9bd44f4751cb76d336fee2c7e25105370025a4e4
Susana Moreno <susana.moreno@hola-internet.com>
2015-03-30 17:01:30 +0200

    Se añade el phpdoc en composer Se añade un controlador con su vista

--- module/Blog/Module.php
+++ module/Blog/Module.php
@@ -1,9 +1,49 @@
 <?php
-/**
- * Doc para la clase
- */
+
 namespace Blog;
 
-class Module
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+/**
+ * Class Module, configuración para el módulo Blog
+ *
+ * @category Blog
+ * @package  Blog
+ * @author   Susana Moreno <susana.ml82@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version  Release: <v0.1>
+ * @link     http://zf2.localhost/blog
+ */
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
+
+    /**
+     * Devuelve un array para pasarle Zend\Loader\AutoloaderFactory.
+     * Esto es para cargar las clases
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    // Autoload all classes from namespace 'Blog' from '/module/Blog/src/Blog'
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                )
+            )
+        );
+    }
+
+    /**
+     * Método que devuelve la configuración del módulo
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
 }
---
+++ module/Blog/src/Blog/Controller/ListController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Blog\Controller;
+use Zend\Mvc\Controller\AbstractActionController;
+
+/**
+ * Class Module, configuración para el módulo Blog
+ *
+ * @category Blog
+ * @package  Blog
+ * @author   Susana Moreno <susana.ml82@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version  Release: <v0.1>
+ * @link     http://zf2.localhost/blog
+ */
+class ListController extends AbstractActionController
+{
+
+}
